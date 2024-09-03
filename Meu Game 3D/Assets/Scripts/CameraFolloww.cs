ce156fbd8240992a3cfeb554c2b064ce6bf1c5d6
teteuMT <silvamatheusje@gmail.com>
2024-09-03 10:17:36 -0300

    3d

--- Meu Game 3D/Assets/Scripts/CameraFolloww.cs
+++ Meu Game 3D/Assets/Scripts/CameraFolloww.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class CameraFolloww : MonoBehaviour
 {
     private Transform alvo;
+
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-       alvo = GameObject.ObejectS 
+        alvo = GameObject.FindWithTag("Plaer").transform;
+        offset = alvo.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = alvo.position - offset;
     }
 }
